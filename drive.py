5fe88c512e5fd600d8e509a63c9b4f6fd4573492
Thorben Krieger <thorben.krieger@gmail.com>
2022-12-05 15:15:15 +0100

    Commit drive.py

--- drive.py
+++ drive.py
@@ -5,14 +5,29 @@ import eventlet
 sio = socketio.Server()
 app = Flask(__name__) #'__main__'
 
-@sio.on('connect') #message, disconnect
-def connect(sid,environ):
-    print('Connected')
 
+@sio.on('*')
+def catch_all(event, data):
+    print(event)
+    
+@sio.event
+def connect():
+    print('Connected')
+    send_control(0,1)  
+     
+#@sio.on('connect') 
+#def connect(sid,environ):
+#    print('Connected')
+#    send_control(0,1)
+
+def send_control(steering_angle, throttle):
+    sio.emit('steer', data = {
+        'steering_angle': steering_angle.__str__(),
+        'throttle' : throttle.__str__()
+    })
 
 
 if __name__ == '__main__':
     app = socketio.Middleware(sio, app)
     eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
 
-    
