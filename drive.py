da56b320e45d04e0b9c94929a16944a2740c7b4a
Thorben Krieger <thorben.krieger@gmail.com>
2022-12-09 09:52:19 +0100

    Commit

--- drive.py
+++ drive.py
@@ -1,24 +1,26 @@
 from flask import Flask
-import socketio
+from flask_socketio import SocketIO
+#import socketio
 import eventlet
 
-sio = socketio.Server()
+#sio = socketio.Server()
 app = Flask(__name__) #'__main__'
+socketio = SocketIO(app)
 
 
-@sio.on('*')
+@socketio.on('*')  
 def catch_all(event, data):
-    print(event)
-    
-@sio.event
-def connect():
+    print("Catch_all")
+
+@socketio.on('connect')  
+def connection(sid, environ):
+    print('connect ', sid)
+
+@socketio.on('connection')  
+#@sio.on('connection') 
+def connect(sid,environ):
     print('Connected')
-    send_control(0,1)  
-     
-#@sio.on('connect') 
-#def connect(sid,environ):
-#    print('Connected')
-#    send_control(0,1)
+    send_control(0,1)
 
 def send_control(steering_angle, throttle):
     sio.emit('steer', data = {
@@ -28,6 +30,8 @@ def send_control(steering_angle, throttle):
 
 
 if __name__ == '__main__':
-    app = socketio.Middleware(sio, app)
-    eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
+    print('Starting app')
+    socketio.run(app)
+   # app = socketio.WSGIApp(sio, app)
+   # eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
 
